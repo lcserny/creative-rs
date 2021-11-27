2c4235a978fed2fc83f58e9cf24fe6fd47a54c7e
Leonardo Cserny <leo.cserny@gmail.com>
2021-11-27 19:10:24 +0200

    WIP

--- src/bin/misc.rs
+++ src/bin/misc.rs
@@ -1,7 +1,22 @@
-use rand::{Rng, thread_rng};
+use log::{debug, error, info, warn};
+use rand::{thread_rng, Rng};
+use std::collections::HashMap;
+use lazy_static::lazy_static;
+
+lazy_static!{
+    static ref DICTIONARY: HashMap<u32, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(11, "food");
+        m.insert(12, "bar");
+        println!("Initialized");
+        m
+    };
+}
 
 fn main() {
     random_generations();
+    logging();
+    lazy_init();
 }
 
 fn random_generations() {
@@ -9,4 +24,18 @@ fn random_generations() {
     println!("{}", rng.gen_range(0..20));
     println!("{}", rng.gen::<f64>());
     println!("{}", if rng.gen() { "Heads" } else { "Tails" });
+}
+
+fn logging() {
+    env_logger::init();
+    error!("Error message");
+    warn!("Use RUST_LOG=debug env var to see this and below messages");
+    info!("Info message");
+    debug!("Debug message");
+}
+
+fn lazy_init() {
+    println!("Started");
+    println!("dict contains: {:?}", *DICTIONARY);
+    println!("dict contains: {:?}", *DICTIONARY);
 }
