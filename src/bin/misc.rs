ca94991891c59f2f8fc35a097d249481d3679b74
Leonardo Cserny <leo.cserny@gmail.com>
2021-12-03 07:49:40 +0200

    did exercises chapter 1

--- src/bin/misc.rs
+++ src/bin/misc.rs
@@ -1,7 +1,7 @@
 use log::{debug, error, info, warn};
 use rand::{thread_rng, Rng};
 use structopt::StructOpt;
-use std::{collections::HashMap, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf, fmt::{Result, self}, num::{ParseIntError, IntErrorKind}, result, error::{Error, self}};
 use lazy_static::lazy_static;
 
 lazy_static!{
@@ -12,6 +12,14 @@ lazy_static!{
         println!("Initialized");
         m
     };
+
+    static ref NUMBERS: Vec<u32> = {
+        let mut n = Vec::new();
+        for i in 1..201 {
+            n.push(i*i);
+        }
+        n
+    };
 }
 
 fn main() {
@@ -21,7 +29,7 @@ fn main() {
     parse_args();
 }
 
-// cargo run -- myfile1.txt -v --result hello.txt
+// cargo run -- myfile1.txt -v --result hello.txt --level 1
 #[derive(StructOpt, Debug)]
 struct Opts {
     /// Activate verbose mode
@@ -35,6 +43,29 @@ struct Opts {
     /// Files to process
     #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
+
+    #[structopt(short = "l", long = "level", parse(try_from_str = parse_range))]
+    level: u8,
+}
+
+#[derive(Debug, Clone)]
+struct MyError {
+    src: String,
+}
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result {
+        write!(f, "invalid number, expected between 1 and 20, provided: {}", self.src)
+    }
+}
+
+fn parse_range(src: &str) -> result::Result<u8, MyError> {
+    if let Ok(n) = src.parse::<u8>() {
+        if n >= 1 && n <= 20 {
+            return Ok(n);
+        }
+    }
+    Err(MyError{src: String::from(src)})
 }
 
 fn parse_args() {
@@ -46,6 +77,11 @@ fn random_generations() {
     println!("{}", rng.gen_range(0..20));
     println!("{}", rng.gen::<f64>());
     println!("{}", if rng.gen() { "Heads" } else { "Tails" });
+
+    for i in 0..10 {
+        let num: f32 = rng.gen_range(100f32..400f32);
+        println!("{}", num);
+    }
 }
 
 fn logging() {
@@ -60,4 +96,6 @@ fn lazy_init() {
     println!("Started");
     println!("dict contains: {:?}", *DICTIONARY);
     println!("dict contains: {:?}", *DICTIONARY);
+
+    println!("{:?}", *NUMBERS);
 }
