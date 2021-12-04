ce91e9bf654cf15322706784f501df6ba956bafa
Leonardo Cserny <leo.cserny@gmail.com>
2021-12-04 19:50:20 +0200

    implemented reading TOML

--- src/toml_static.rs
+++ src/toml_static.rs
@@ -1,14 +1,52 @@
+use std::{env, fs};
+
 use serde_derive::Deserialize;
 
 #[allow(unused)]
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Input {
     xml_file: String,
     json_file: String,
 }
 
-// TODO: rest of the structs and code
+#[allow(unused)]
+#[derive(Deserialize, Debug)]
+struct Redis {
+    host: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize, Debug)]
+struct Sqlite {
+    db_file: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize, Debug)]
+struct Postgresql {
+    username: String,
+    password: String,
+    host: String,
+    port: String,
+    database: String,
+}
+
+#[allow(unused)]
+#[derive(Deserialize, Debug)]
+struct Config {
+    input: Input,
+    redis: Redis,
+    sqlite: Sqlite,
+    postgresql: Postgresql,
+}
 
 pub fn read() {
-    
+    let config_const_values: Config = {
+        let config_path = env::args().nth(1).unwrap();
+        let config_text = fs::read_to_string(&config_path).unwrap();
+        toml::from_str(&config_text).unwrap()
+    };
+
+    println!("Original: {:#?}", config_const_values);
+    println!( "[Postgresql].Database: {}", config_const_values.postgresql.database );
 }
